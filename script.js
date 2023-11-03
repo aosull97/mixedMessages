3be0486c4896deb01127095e28c9678cff03da83
aosull97 <149597356+aosull97@users.noreply.github.com>
2023-11-03 17:01:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const goodMessage = ['An exciting opportunity lies ahead of you', 'A weight will be taken off your mind', 'All your hard work will soon pay off', 'Happy news is on its way'];
+
+  const beAwareMessage = ['You will be hungry again in an hour', 'A piece of clothing will be lost in the wash', 'A challenge lays ahead', 'Someone will dissapoint you'];
+
+  const generalMessage = ['Dont worry about money. The best things in life are free', 'The early bird gets the worm, but the second mouse gets the cheese', 'If you look back, youll soon be going that way', 'If you want the rainbow, you have to tolerate the rain.'];
+
+  const randomMessage = () => {
+    let randomGood = goodMessage[Math.floor(Math.random()*goodMessage.length)]
+    let randomBeAware = beAwareMessage[Math.floor(Math.random()*beAwareMessage.length)]
+    let randomGeneral = generalMessage[Math.floor(Math.random()*generalMessage.length)]
+    console.log("You're good fortune is: " + randomGood)
+    console.log("Something to be aware of: " + randomBeAware)
+    console.log("A message just for you: " + randomGeneral)
+  };
+    
+
+
+
